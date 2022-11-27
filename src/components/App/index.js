66a127db04a398da34e01b5c6612ebf4dab59363
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2022-11-27 14:08:13 +0200

    del

--- src/components/App/index.js
+++
@@ -1,9 +0,0 @@
-import '../Profile/Profile';
-import '../Statistic/Statistic';
-import '../FriendList/FriendList';
-import '../TransactionHistory/TransactionHistory';
-
-import user from 'datas/user.json';
-import stat from 'datas/data.json';
-import friends from 'datas/friends.json';
-import transactions from 'datas/transactions.json';
